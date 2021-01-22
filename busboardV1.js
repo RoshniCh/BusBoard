05da4f7af31b6ae614afe4a12c59c08784bce539
RoshniCh <roshni.chandrasekar@softwire.com>
2021-01-22 13:36:43 +0000

    print json

---
+++ busboardV1.js
@@ -0,0 +1,19 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+
+var request = new XMLHttpRequest()
+
+var reqArray 
+var url = 'https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals'
+request.open('GET', url, true)
+request.onreadystatechange = function () 
+{
+     if (request.readyState === 4) 
+     {
+      console.log(request.status);
+      reqArray = JSON.parse(request.responseText)
+      console.log(reqArray);
+     }
+}
+request.send()
+
+reqArray
